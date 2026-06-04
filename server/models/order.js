151b63066b48f1b2cc689996d12ec13ed4b1368d
Atif-uddin <uddinatif34@gmail.com>
2026-06-05 00:02:40 +0530

    order-model

--- server/models/order.js
+++ server/models/order.js
@@ -1,5 +1,48 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items:[{
+        product:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity:{
+            type: Number,
+            required: true,
+            min: 1
+        },
+        price:{
+            type: Number,
+            required: true,
+            min: 0
+        }
+    }],
+    totalAmount:{
+        type: Number,
+        required: true,
+        min: 0
+    },
+    orderStatus:{
+        type: String,
+        enum:[
+            "pending",
+            "processing",
+            "shipped",
+            "delivered",
+            "cancelled"
+        ],
+        default: "pending"
+    }
+},{
+    timestamps: true
 })
+
+const Order = mongoose.model('Order', orderSchema)
+
+export default Order
